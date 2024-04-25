f90c2aef9bca0da40b3f65ee9a566c05e4fcf7be
VanessaGL7 <glvanessa2@gmail.com>
2024-04-24 19:12:48 -0600

    s

---
+++ Book/components/Background_one.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Image} from '@gluestack-ui/themed';
+
+const Background_one = () => {
+    return <Image source={
+        require("../assets/images/Login2.png")
+    }alt="bg" />
+};
+
+export default Background_one;
